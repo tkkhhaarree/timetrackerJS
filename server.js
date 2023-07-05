0e11bffe94eaa317308484865b49dcff2bec9dd1
DESKTOP-1TCM56D\user <tarunkhare54321@gmail.com>
2023-07-05 18:18:59 +0530

    render changes5

--- server.js
+++ server.js
@@ -20,14 +20,6 @@ app.use("/usersession", require("./routes/usersession"));
 app.use("/stats", require("./routes/stats"));
 app.use("/urlcategory", require("./routes/urlcategory"));
 
-if (process.env.NODE_ENV === "production") {
-   app.use(express.static("client/build"));
-
-   app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-   });
-}
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
